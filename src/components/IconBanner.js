3f2609c0ffd4d5712952f63e67d0cea470ce5847
solitdio079 <solitdio079@gmail.com>
2026-02-10 15:00:50 +0300

    Reduce sliders

--- src/components/IconBanner.js
+++ src/components/IconBanner.js
@@ -8,7 +8,7 @@ import br6 from "../images/br6.png"
 export default function IconBanner(){
     const images = [br1,br2,br3,br4,br5,br6]
     return(<>
-    <div className="bg-gray-900 text-white my-12 p-10 flex flex-col gap-6">
+    <div className="bg-gray-900 text-white max-w-screen my-12 p-10 flex flex-col gap-6">
         <h3 className="text-xl lg:text-3xl">More Than <span className="text-info">52.0 K</span> Brand with Work Here.</h3>
         <hr className="bg-gray-200"/>
         <div className="flex gap-6 justify-between animate-scroll">
